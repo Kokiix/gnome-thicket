ea4af3b9ee0c49fb5b13a220f0c9e2287e01a939
Kenneth <kokiidoki@gmail.com>
2025-10-10 09:33:05 -0400

    color hexes, small tweaks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const HEX_GRID = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("white");
+  background("black");
   draw_board();
 }
 
@@ -11,20 +11,32 @@ function draw() {
 
 function draw_board() {
   const HEX_RADIUS_MAGIC_NUMBER = 20;
+  // Distance to vertex
   const circumradius = windowWidth / HEX_RADIUS_MAGIC_NUMBER;
+  // Distance to edge
   const inradius = circumradius * sqrt(3) / 2;
   const board_vert_offset = (windowHeight - inradius * 14) / 2 + inradius;
 
-  // # of tiles in q-dimension goes from 4 to 7 to 4
-  let board_q_grow_amt = 1;
+  strokeWeight(circumradius / HEX_RADIUS_MAGIC_NUMBER * 2);
+  stroke(51, 102, 68);
+  fill(87, 135, 75);
+
+  // Using axial coordinates https://www.redblobgames.com/grids/hexagons/#coordinates-axial
+  // # of tiles on q-axis goes from 4 to 7 to 4
+  let col_index = 0;
+  let q_grow_amt = 1;
   for (let q_length = 4; 
     q_length <= 7 && q_length >=4; 
-    q_length += board_q_grow_amt) {
+    q_length += q_grow_amt) {
+
+    HEX_GRID[col_index] = [];
     
-    // Iterate down the q axis
+    // Given each length, iterate down q axis
     const col_offset = board_vert_offset + (7 - q_length) * inradius;
     for (let q = 0; q < q_length; q++) {
-      const center_x = windowWidth / 2 + (7 - q_length) * board_q_grow_amt * circumradius * 2 * 0.75;
+      HEX_GRID[col_index][q] = new HexTile();
+      
+      const center_x = windowWidth / 2 + (7 - q_length) * q_grow_amt * circumradius * 2 * 0.75;
       const center_y = col_offset + inradius * q * 2;
       beginShape();
       for (let angle = 0; angle < 6.28; angle += PI / 3) {
@@ -33,10 +45,10 @@ function draw_board() {
           round(center_y + circumradius * sin(angle)));
       }
       endShape(CLOSE);
-    }   
+    }
 
     if (q_length == 7) {
-      board_q_grow_amt = -1;
+      q_grow_amt = -1;
     }
   }
 }
