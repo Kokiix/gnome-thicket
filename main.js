020d43f71a1f5030cbaf5a2ec4324a207190f4c5
Kenneth <kokiidoki@gmail.com>
2025-10-10 09:54:42 -0400

    detect click, convert to questionable q and r

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const HEX_GRID = [];
+const DIMENSIONS = {"HEX_RADIUS_MAGIC_NUMBER": 20};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
+  DIMENSIONS.circumradius = windowWidth / DIMENSIONS.HEX_RADIUS_MAGIC_NUMBER;
+  DIMENSIONS.inradius = DIMENSIONS.circumradius * sqrt(3) / 2;
+  DIMENSIONS.board_vert_offset = (windowHeight - DIMENSIONS.inradius * 14) / 2 + DIMENSIONS.inradius;
   draw_board();
 }
 
 function draw() {
 }
 
-function draw_board() {
-  const HEX_RADIUS_MAGIC_NUMBER = 20;
-  // Distance to vertex
-  const circumradius = windowWidth / HEX_RADIUS_MAGIC_NUMBER;
-  // Distance to edge
-  const inradius = circumradius * sqrt(3) / 2;
-  const board_vert_offset = (windowHeight - inradius * 14) / 2 + inradius;
+function mouseClicked() {
+  let testX = mouseX - windowWidth / 2;
+  let testY = mouseY - windowWidth / 2;
+  let q_clicked = (2/3 * testX) / DIMENSIONS.circumradius;
+  let r_clicked = (-1/3 * testX + sqrt(3)/3 * testY) / DIMENSIONS.circumradius;
+  console.log(`${q_clicked}, ${r_clicked}`);
+}
 
-  strokeWeight(circumradius / HEX_RADIUS_MAGIC_NUMBER * 2);
+function draw_board() {
+  strokeWeight(DIMENSIONS.circumradius / DIMENSIONS.HEX_RADIUS_MAGIC_NUMBER * 2);
   stroke(51, 102, 68);
   fill(87, 135, 75);
 
@@ -32,17 +37,17 @@ function draw_board() {
     HEX_GRID[col_index] = [];
     
     // Given each length, iterate down q axis
-    const col_offset = board_vert_offset + (7 - q_length) * inradius;
+    const col_offset = DIMENSIONS.board_vert_offset + (7 - q_length) * DIMENSIONS.inradius;
     for (let q = 0; q < q_length; q++) {
       HEX_GRID[col_index][q] = new HexTile();
-      
-      const center_x = windowWidth / 2 + (7 - q_length) * q_grow_amt * circumradius * 2 * 0.75;
-      const center_y = col_offset + inradius * q * 2;
+
+      const center_x = windowWidth / 2 + (7 - q_length) * q_grow_amt * DIMENSIONS.circumradius * 2 * 0.75;
+      const center_y = col_offset + DIMENSIONS.inradius * q * 2;
       beginShape();
       for (let angle = 0; angle < 6.28; angle += PI / 3) {
         vertex(
-          round(center_x + circumradius * cos(angle)), 
-          round(center_y + circumradius * sin(angle)));
+          round(center_x + DIMENSIONS.circumradius * cos(angle)), 
+          round(center_y + DIMENSIONS.circumradius * sin(angle)));
       }
       endShape(CLOSE);
     }
