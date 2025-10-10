4fa969d525269e33a1506e3bbc9b48a207a59230
Kenneth <kokiidoki@gmail.com>
2025-10-10 01:37:15 -0400

    row of hexies!

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const HEX_GRID = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("black");
+  background("white");
   draw_board();
 }
 
@@ -10,22 +10,24 @@ function draw() {
 }
 
 function draw_board() {
-  const VERTICAL_MARGIN = windowWidth / 5;
-  const RADIUS = windowWidth / 5;
+  const CIRCUMRAD = windowWidth / 25;
+  const INRADIUS = CIRCUMRAD * sqrt(3) / 2
+  const VERT_OFFSET = (windowHeight - INRADIUS * 14) / 2 + INRADIUS;
+  console.log(VERT_OFFSET);
+  
+  
+  for (let i = 0; i < 7; i++) {
+    let center_x = windowWidth / 2;
+    let center_y = VERT_OFFSET + INRADIUS * i * 2;
+    beginShape();
+    for (let angle = 0; angle < 6.28; angle += PI / 3) {
+      vertex(
+        round(center_x + CIRCUMRAD * cos(angle)), 
+        round(center_y + CIRCUMRAD * sin(angle)));
+    }
+    endShape(CLOSE);
 
-  console.log(windowWidth, windowHeight);
-
-  let center_x = windowWidth / 2;
-  let center_y = VERTICAL_MARGIN + RADIUS;
-  beginShape();
-  for (let angle = 0; angle < 6.28; angle += Math.PI / 3) {
-
-    // console.log(angle);
-    console.log(Math.round(center_x + Math.cos(angle)), Math.round(center_y + Math.sin(angle)));
-    vertex(Math.round(center_x + RADIUS * Math.cos(angle)), Math.round(center_y + RADIUS * Math.sin(angle)));
   }
-  endShape(CLOSE);
-
 
   // # of tiles in q-dimension goes from 4 to 7 to 4
   // let board_q_grow = true;
