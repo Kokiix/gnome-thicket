85027d4f2d8b1a62f0d309f1f8d507dc5e3c2997
Kenneth <kokiidoki@gmail.com>
2025-10-10 10:52:20 -0400

    convert to rounded coordinates

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const DIMENSIONS = {"HEX_RADIUS_MAGIC_NUMBER": 20};
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
-  DIMENSIONS.circumradius = windowWidth / DIMENSIONS.HEX_RADIUS_MAGIC_NUMBER;
+  DIMENSIONS.circumradius = width / DIMENSIONS.HEX_RADIUS_MAGIC_NUMBER;
   DIMENSIONS.inradius = DIMENSIONS.circumradius * sqrt(3) / 2;
-  DIMENSIONS.board_vert_offset = (windowHeight - DIMENSIONS.inradius * 14) / 2 + DIMENSIONS.inradius;
+  DIMENSIONS.board_vert_offset = (height - DIMENSIONS.inradius * 14) / 2 + DIMENSIONS.inradius;
   draw_board();
 }
 
@@ -14,11 +14,23 @@ function draw() {
 }
 
 function mouseClicked() {
-  let testX = mouseX - windowWidth / 2;
-  let testY = mouseY - windowWidth / 2;
-  let q_clicked = (2/3 * testX) / DIMENSIONS.circumradius;
-  let r_clicked = (-1/3 * testX + sqrt(3)/3 * testY) / DIMENSIONS.circumradius;
-  console.log(`${q_clicked}, ${r_clicked}`);
+  let x = mouseX - width / 2;
+  let y = mouseY - height / 2;
+  let q_fractional = (2/3 * x) / DIMENSIONS.circumradius;
+  let r_fractional = (-1/3 * x + sqrt(3)/3 * y) / DIMENSIONS.circumradius;
+  let s_fractional = -q_fractional - r_fractional;
+  let q_rounded = round(q_fractional);
+  let r_rounded = round(r_fractional);
+  let s_rounded = round(s_fractional);
+  let q_diff = abs(q_rounded - q_fractional);
+  let r_diff = abs(r_rounded - r_fractional);
+  let s_diff = abs(s_rounded - s_fractional);
+  if (q_diff > r_diff && q_diff > s_diff) {
+    q_rounded = -r_rounded - s_rounded;
+  } else if (r_diff > q_diff && r_diff > s_diff) {
+    r_rounded = -q_rounded - s_rounded;
+  }
+  console.log(q_rounded, r_rounded);
 }
 
 function draw_board() {
@@ -41,7 +53,7 @@ function draw_board() {
     for (let q = 0; q < q_length; q++) {
       HEX_GRID[col_index][q] = new HexTile();
 
-      const center_x = windowWidth / 2 + (7 - q_length) * q_grow_amt * DIMENSIONS.circumradius * 2 * 0.75;
+      const center_x = width / 2 + (7 - q_length) * q_grow_amt * DIMENSIONS.circumradius * 2 * 0.75;
       const center_y = col_offset + DIMENSIONS.inradius * q * 2;
       beginShape();
       for (let angle = 0; angle < 6.28; angle += PI / 3) {
